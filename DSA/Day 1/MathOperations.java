3493017e87ff06a17ed4909c7c239db2804b9029
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-03-21 09:59:48 +0530

    DSA - Day 1 -  Math Operations

---
+++ DSA/Day 1/MathOperations.java
@@ -0,0 +1,12 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        System.out.println("Max: " + Math.max(10,17));
+        System.out.println("Min: " + Math.min(10,17));
+        System.out.println("Floor: " + (int)Math.floor(16.2));
+        System.out.println("Ceiling: " + Math.ceil(16.2));
+        System.out.println("Power: " + Math.pow(2,3));
+        System.out.println("Square Root: " + Math.sqrt(16));
+        System.out.println("Round: " + Math.round(18.6));
+        System.out.println("Absolute: " + Math.abs(-5));
+    }
+}
