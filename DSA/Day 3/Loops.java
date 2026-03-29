02cd8cd50f8bc6edd4e14f63c41afd2455ebfe1b
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-03-29 09:45:08 +0530

    Add Loops class with commented examples of loop structures

---
+++ DSA/Day 3/Loops.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter n: ");
+        // int n = sc.nextInt();
+
+        // // For Loop
+        // for (int i = 0; i <= n; i++) {
+        // System.out.println(i);
+        // }
+
+        // // While Loop
+        // int i = 0;
+        // while (i <= n) {
+        // System.out.println(i);
+        // i++;
+        // }
+
+        // Do-While Loop
+        // int i = 0;
+        // do{
+        // System.out.println(i);
+        // i++;
+        // }while(i <= n);
+
+    }
+}
